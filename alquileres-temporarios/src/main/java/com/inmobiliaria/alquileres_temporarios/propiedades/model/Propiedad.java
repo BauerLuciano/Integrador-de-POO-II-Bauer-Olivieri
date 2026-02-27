d7de97db189bc63c8c247d15b85414f3acb31129
RckOlv <yosoyricky9@gmail.com>
2026-02-27 20:52:35 -0300

    feat(propiedades): implementar relacion ManyToOne con Propietario (HU-04)

--- alquileres-temporarios/src/main/java/com/inmobiliaria/alquileres_temporarios/propiedades/model/Propiedad.java
+++ alquileres-temporarios/src/main/java/com/inmobiliaria/alquileres_temporarios/propiedades/model/Propiedad.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.inmobiliaria.alquileres_temporarios.propietarios.model.Propietario;
+
 @Entity
 @Data 
 public class Propiedad {
@@ -17,15 +19,13 @@ public class Propiedad {
     private Double porcentajeDeposito;
     private String politicaCancelacion;
 
-    // Aquí Bauer (tu amigo) inyectará el Propietario en la HU-04
-    // @ManyToOne ... private Propietario propietario;
+    @ManyToOne(fetch = FetchType.EAGER) 
+    @JoinColumn(name = "propietario_id")
+    private Propietario propietario;
 
-    // Para la Iteración 1, usaremos una lista simple de bloqueos
-    // (En la base de datos esto se mapeará con herencia en la HU-05/07)
     @Transient 
     private List<BloqueoCalendario> bloqueos = new ArrayList<>();
 
-    // HU-06: El motor de disponibilidad
     public boolean estaDisponible(java.time.LocalDate inicio, java.time.LocalDate fin) {
         return bloqueos.stream().noneMatch(b -> b.seSolapaCon(inicio, fin));
     }
