8ecf8e9b1cf08f8b06b2591fd8ad79e1fbf394c1
RckOlv <yosoyricky9@gmail.com>
2026-02-27 19:41:20 -0300

    Se creo el proyecto con maven

---
+++ alquileres-temporarios/src/main/java/com/inmobiliaria/alquileres_temporarios/AlquileresTemporariosApplication.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.alquileres_temporarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquileresTemporariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquileresTemporariosApplication.class, args);
+	}
+
+}
---
+++ alquileres-temporarios/src/test/java/com/inmobiliaria/alquileres_temporarios/AlquileresTemporariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.alquileres_temporarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquileresTemporariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
